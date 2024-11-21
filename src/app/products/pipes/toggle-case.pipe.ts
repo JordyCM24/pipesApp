68d6e861028cee13debb0029cb8a2441dd372593
JordyCM24 <jordycalderon215@gmail.com>
2024-11-20 21:56:28 -0500

    argumentos en pipes

--- src/app/products/pipes/toggle-case.pipe.ts
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -5,9 +5,13 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class toggleCasePipe implements PipeTransform {
     
-    transform(value: string): string {
-    
-        return value.toUpperCase();
+    transform(value: string, toUpper: boolean = false): string {
+
+        console.log({value, toUpper});
+        
+        return (toUpper) 
+            ? value.toUpperCase() 
+            : value.toLowerCase();
     }
 
 }
